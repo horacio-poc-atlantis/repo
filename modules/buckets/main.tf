413ff6fd0cd1350b0f7fb0d947254d7e8038f07f
Horacio Guillermo de Oro <hgdeoro@Horacios-MBP.lan>
2023-11-21 16:40:08 +0100

    Update

--- modules/buckets/main.tf
+++ modules/buckets/main.tf
@@ -1,7 +1,11 @@
 resource "null_resource" "some-bucket" {
 
   provisioner "local-exec" {
-    command = "echo environment is ${var.environment}"
+    command = "echo environment is ${var.environment} - bucket name is ${var.bucket_name}-${var.environment}"
   }
 
+}
+
+output "bucket-name" {
+  value = "${var.bucket_name}-${var.environment}"
 }
--- some-application/main.tf
+++ some-application/main.tf
@@ -1,11 +1,12 @@
 module "app-buckets" {
   source = "../modules/buckets"
   environment = var.environment
+  bucket_name = "my-app"
 }
 
-module "app-database" {
-  source = "../modules/database"
-  environment = var.environment
-}
+#module "app-database" {
+#  source = "../modules/database"
+#  environment = var.environment
+#}
 
 # touch 1
